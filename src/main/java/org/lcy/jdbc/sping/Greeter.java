e07cbf059c56ca8c7813ec564d2499e642e97caf
Lee <lcy>
2019-04-05 10:06:22 +0900

    수정

--- src/main/java/org/lcy/jdbc/sping/Greeter.java
+++ src/main/java/org/lcy/jdbc/sping/Greeter.java
@@ -1,7 +1,18 @@
 package org.lcy.jdbc.sping;
 
 public class Greeter {
-	public String greet() {
-		return "Hello, World!";
+
+	/**
+	 * Default Constructor
+	 */
+	public Greeter() {
+		System.out.println("Greeter instance created.");
+	}
+
+	/**
+	 * "Hello, World!"를 출력한다.
+	 */
+	public void greet() {
+		System.out.println("Hello, World!");
 	}
 }
--- src/main/java/org/lcy/jdbc/sping/SpingMain.java
+++ src/main/java/org/lcy/jdbc/sping/SpingMain.java
@@ -3,14 +3,25 @@ package org.lcy.jdbc.sping;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * p.46 [리스트 2.5]와 p.50 [리스트 2.6] Main 클래스 수정본
+ * 
+ * @author Jacob
+ */
 public class SpingMain {
 
-		public static void main(String[] args) {
-			ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
-					"chap2.xml");
-			Greeter g = ctx.getBean("greeter", Greeter.class);
-			System.out.println(g.greet());
-			ctx.close();
-		}
-	}
+	public static void main(String[] args) {
+		// 클래스 패스에서 chap02.xml 파일을 읽어서 컨텍스트 초기화
+		// ctx 에는 chap02.xml에서 정의한 모든 빈들이 들어있다.
+		ConfigurableApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"chap2.xml");
+		Greeter g1 = ctx.getBean("greeter", Greeter.class);
+		g1.greet();
 
+		Greeter g2 = ctx.getBean("greeter", Greeter.class);
+		g2.greet();
+
+		System.out.println(g1 == g2);
+		ctx.close();
+	}
+}
